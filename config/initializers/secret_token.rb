2cbfa1c68da9bbc2c010c64ac98d44d30d2a1578
Ron Colton <ron.colton@gmail.com>
2011-04-23 12:49:58 -0700

    Finished Layouts and Routes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'cf35e2515cdec90949bc31f59dabf5539fda1647b8f74d72704679f0b4b33bac05d7e166031fd74edc7aabe438863e92e84138f0b9cef71661a9e7fc2204f9ec'
+SampleApp::Application.config.secret_token = '035b79d61248e14aa5122713c4cfd59ca1ed788eacbd699aa1e84a9d05aadf8498157e3a5786e9538f73c1b850d67820f690684cde120c319938786540f6cbee'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 SampleApp::Application.routes.draw do
-  match '/contact', 	:to => 'pages#contact'
-  match '/about', 		:to => 'pages#about'
-  match '/help',			:to => 'pages#help'
-  
-  root :to => 'pages#home'
+
+  get "users/new"
+
+	root :to => 'pages#home'
+	
+	match '/signup',	:to => 'users#new'
+	
+	match '/home', 		:to => 'pages#home'
+ 	match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help' 
+
+end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -61,4 +69,3 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-end
